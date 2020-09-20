1d6001bf8c9714e9ed723c9901e6300cd2bd00b2
Ian Edwards <ian.edward@uconn.edu>
2020-09-20 09:14:54 -0400

    Update
    Got JSON to log to console

--- json-display-test/src/App.js
+++ json-display-test/src/App.js
@@ -1,38 +1,47 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import axios from 'axios'
+// import ReactDOM from 'react-dom'
+// import axios from 'axios'
 
 class App extends React.Component {
 
-  constructor(props) {
-    super(props)
-    this.state = {
-      lists: []
+    constructor(props) {
+        super(props)
+        this.state = {
+            lists: []
+        }
     }
-  }
 
-  componentDidMount() {
-    axios.get('http://localhost:3001/api/v1/lists.json')
-    .then(response => {
-      console.log(response.data);
-      this.setState({lists: response.data})
-    })
-  }
+    componentDidMount() {
+        //const url = "https://jsonplaceholder.typicode.com/posts";
+        //const url = "https://gist.githubusercontent.com/rominirani/8235702/raw/a50f7c449c41b6dc8eb87d8d393eeff62121b392/employees.json"
+        const url = "https://raw.githubusercontent.com/ianedwards-jpg/Homework-19/master/user_directory_app/public/employees.json"
+        fetch(url, {
+            method: "GET"
+        }).then(response => response.json()).then(lists => {
+            this.setState({ lists: lists.Employees })
+            console.log(lists.Employees)
+        })
 
-  render() {
-    return(
-      <div>
-        {this.state.lists.map((list) => {
+    }
+
+    render() {
+        return (
+            <div>
+                {/* {this.state.lists.map((lists) => {
           return(
-            <div key={list.id}>
-              <h1>{list}</h1>
+            <div key={lists.id}>
+              <h3>{JSON.stringify(lists.Employees)}</h3>
             </div>
           )
-        })}
-      </div>
-    )
-  }
-} 
+        })} */}
+
+                <div>
+                    <h1>Hello World!</h1>
+                </div>
+            </div>
+        )
+    }
+}
 export default App;
 
 
@@ -40,7 +49,7 @@ export default App;
 // import Example1 from './Example1';
 // import Example2 from './Example2';
 // import Example3 from './Example3';
- 
+
 // class App extends Component {
 //     render() {
 //         return (
@@ -52,3 +61,11 @@ export default App;
 //         );
 //     }
 // }
+
+//   componentDidMount() {
+//     axios.get('http://localhost:3001/api/v1/lists.json')
+//     .then(response => {
+//       console.log(response.data);
+//       this.setState({lists: response.data})
+//     })
+//   }
